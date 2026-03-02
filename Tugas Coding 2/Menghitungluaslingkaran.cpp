8c3ef3f2203d1424196e76ec75e40bcfe95e5932
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-03-02 16:40:13 +0700

    merapikan bagian fungsi baris ke 4 menjadi "fungsi menghitung luas"

---
+++ Tugas Coding 2/Menghitungluaslingkaran.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// fungsi menghitung luas
+double hitungLuas(double r) {
+    const double PHI = 3.14159;
+    return PHI * r * r;
+}
+
+int main() {
+    double jari;
+    
+    // prosedur input
+    cout << "Masukkan jari-jari lingkaran: ";
+    cin >> jari;
+    
+    // panggil fungsi hitung luas
+    double luas = hitungLuas(jari);
+    
+    // prosedur output
+    cout << "Luas lingkaran adalah: " << luas << endl;
+    
+    return 0;
+}
